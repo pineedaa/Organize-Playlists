715b28d10c538df00793979b3d0254b37c1dc0ed
Carlos Pineda <crevi2002@gmail.com>
2024-04-24 21:32:40 +0200

    Deleted debug echo

--- organize-playlist.sh
+++ organize-playlist.sh
@@ -47,7 +47,6 @@ fi
 for file in "$src_dir"/*; do
   if [ -f "$file" ]; then
     if [[ $(file --mime-type "$file" | grep -q "audio") && $(file --mime-type "$file" | grep -q "video") ]]; then
-      echo "not audio"
       continue
     fi
 
